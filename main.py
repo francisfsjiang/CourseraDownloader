d1b701f67656f84c96d87d3f31558def20cb424e
Neveralso <neveralso@gmail.com>
2014-11-28 21:13:26 +0800

    commit

--- main.py
+++ main.py
@@ -2,24 +2,26 @@
 import requests
 import bs4
 import os
+from pprint import pprint
 from multiprocessing import get_context
 # from multiprocessing import get_context,Process
 # from multiprocessing.queues import JoinableQueue
 
-from pprint import pprint
+from bs4 import BeautifulSoup, element
 
 
-def kubi_worker(queue, download_dir, session):
+def kubi_worker(queue, download_dir, header, subtitles_lang):
     while not queue.empty():
         item = queue.get()
 
-        pprint(item)
+        # pprint(item)
         file_name = str(item['id']) + ' - ' + item['name'] + '.' + item['type']
         file_path = os.path.join(download_dir, file_name)
-
         print("Start downloading " + file_path)
         if item['type'] == 'mp4':
-            download_video(item['url'], file_path, session)
+            session = requests.Session()
+            session.headers = header
+            download_video(item['url'], file_path, session, subtitles_lang)
         else:
             download_file(item['url'], file_path)
 
@@ -27,25 +29,44 @@ def kubi_worker(queue, download_dir, session):
         queue.task_done()
 
 
-def download_video(url, file_path, session):
+def download_video(url, file_path, header, subtitles_lang):
     response = session.get(url, verify=True)
-    print(session.headers)
-    soup = bs4.BeautifulSoup(response.content.decode())
+    # print(response.request.headers)
+    # print(dict(response.headers))
+    # print(response.content.decode())
+    soup = BeautifulSoup(response.content.decode())
     video_address = soup.video.source['src']
-
+    print(video_address)
     download_file(video_address, file_path, session)
+    #
+    # subitles = soup.video.source.source.descendants
+    subtitles = dict()
+    for i in soup.video.source.source.descendants:
+        if type(i) != element.NavigableString:
+            subtitles[i['srclang']] = i['src']
+            # print(i['src'], '+++++', i['srclang'])
 
-    subitles = soup.video.source.source.descendants
+    subtitles_chosen = None
+    for i in subtitles_lang:
+        if i in subtitles:
+            subtitles_chosen = subtitles[i]
+    if subtitles_chosen:
+        print(subtitles_chosen)
+        download_file(subtitles_chosen, file_path.replace('.mp4', '.srt'))
 
 
 def download_file(url, file_path, session=requests.Session()):
+    if os.path.exists(file_path):
+        return
+    file_path = file_path + '.temp'
     r = session.get(url, stream=True)
     with open(file_path, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=512*1024):
+        for chunk in r.iter_content(chunk_size=0x100000):
             if chunk:
                 print(file_path + ' downloaded 1MB')
                 f.write(chunk)
                 f.flush()
+    os.rename(file_path, file_path.replace('.temp', ''))
 
 
 def get_request_header():
@@ -57,7 +78,7 @@ def get_request_header():
         # print(i)
         key, item = i[0], i[1]
         # print(key, item)
-        if key in ['User-Agent', 'Accept', 'Accept-Language', 'Accept-Encoding', 'Referer', 'Cookie', 'Connection', 'Cache-Control']:
+        if key in ['User-Agent', 'Accept', 'Accept-Language', 'Accept-Encoding', 'Cookie', 'Connection']:
             header[key] = item
     return header
 
@@ -70,7 +91,7 @@ def generate_tasks(task_queue, lecture_list):
         # print(name)
         # pdf_tag = i.find(title='PDF')
         # ppt_tag = i.find(title='PPT')
-        video_tag = i.find(title='Video (MP4)')
+        video_tag = i.find(name='a', attrs='lecture-link')
         # if pdf_tag:
         #     task = {
         #         'id': id,
@@ -94,7 +115,7 @@ def generate_tasks(task_queue, lecture_list):
                 'id': id,
                 'name': name,
                 'type': 'mp4',
-                'url': video_tag['href']
+                'url': video_tag['data-modal-iframe']
             }
             id += 1
             task_queue.put(task)
@@ -104,7 +125,7 @@ def generate_tasks(task_queue, lecture_list):
 
 if __name__ == '__main__':
     course_id = 'ml-007'
-    srt_lang = 'zh'
+    subtitles_lang = ['zh-cn', 'zh', 'en']
     login_page_url = 'https://accounts.coursera.org/'
     lecture_list_url = 'https://class.coursera.org/%s/lecture' % course_id
     login_url = ''
@@ -139,15 +160,15 @@ if __name__ == '__main__':
     generate_tasks(task_queue, lecture_list)
     # print(task_queue.qsize())
 
-    # workers = []
-    # for i in range(os.cpu_count()):
-    #     p = contex.Process(target=kubi_worker, args=(task_queue, download_dir, session,))
-    #     p.start()
-    #     workers.append(p)
-    # while True:
-    #     pprint(task_queue.get())
+    workers = []
+    for i in range(2):
+        p = contex.Process(target=kubi_worker, args=(task_queue, download_dir, header, subtitles_lang))
+        p.start()
+        workers.append(p)
     while True:
-        kubi_worker(task_queue, download_dir, session)
+        pprint(task_queue.get())
+    # while True:
+    #     kubi_worker(task_queue, download_dir, session, subtitles_lang)
 
     task_queue.join()
     exit(0)
